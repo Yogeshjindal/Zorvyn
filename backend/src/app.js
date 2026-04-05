cd11d4eea0dbbe5d81006dc7ec4ef1c92e7d8345
Yogesh <yogesh1jindall@gmail.com>
2026-04-05 07:05:56 +0530

    allowing access

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,8 +20,8 @@ app.use(helmet());
 
 // CORS
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true,
+  origin: '*',
+  credentials: false,
 }));
 
 // Body parsing
